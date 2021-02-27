07313b9bf7675b73a8ba6365236736fac3b8b643
Darien Sandifer <dsandif@charter.net>
2021-02-26 18:58:28 -0600

    Initial Commit

---
+++ Shared/StockTileApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTileApp.swift
+//  Shared
+//
+//  Created by Darien Sandifer on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct StockTileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
